aed764c9a61bbfec77a94eca9ee24ddf3ae86b2e
alekx <alekx-gr@yandex.by>
2022-01-23 20:36:56 +0300

    added base class structures

--- src/com/File.java
+++ src/com/File.java
@@ -1,4 +1,27 @@
 package com;
 
 public class File {
+    private String fileName;
+    private String filenameExtension;
+
+    public File(String fileName, String filenameExtension) {
+        this.fileName = fileName;
+        this.filenameExtension = filenameExtension;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilenameExtension() {
+        return filenameExtension;
+    }
+
+    public void setFilenameExtension(String filenameExtension) {
+        this.filenameExtension = filenameExtension;
+    }
 }
--- src/com/Folder.java
+++ src/com/Folder.java
@@ -1,4 +1,21 @@
 package com;
 
 public class Folder {
+    private String folderName;
+
+    public Folder (){
+        this.folderName = "Новая папка";
+    }
+
+    public Folder (String name) {
+        this.folderName = name;
+    }
+
+    public String getName() {
+        return folderName;
+    }
+
+    public void setName(String name) {
+        this.folderName = name;
+    }
 }
